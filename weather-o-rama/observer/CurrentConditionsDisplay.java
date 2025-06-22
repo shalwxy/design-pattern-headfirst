285a7da1baa6bb395b8559e7d9b5da1046348199
shalwxy <shalwxy@github.com>
2025-06-22 19:21:27 +0900

    feat: add observer pattern

---
+++ weather-o-rama/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,30 @@
+package observer;
+
+import subject.Subject;
+import subject.WeatherData;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private Subject subject;
+
+    public CurrentConditionsDisplay(Subject subject) {
+        this.subject = subject;
+        subject.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("현재 상태: 온도 %f 습도 %f 압력 %f\n", temperature, humidity, pressure);
+    }
+
+}
