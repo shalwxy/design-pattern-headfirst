7579a703a1840a41fb2849f6b7cd67945c847d9f
shalwxy <shalwxy@gmail.com>
2025-11-09 17:03:26 +0900

    fix: command pattern - use lambda expression

--- command/remote_control/RemoteLoader.java
+++ command/remote_control/RemoteLoader.java
@@ -17,10 +17,10 @@ public class RemoteLoader {
         GarageDoor garageDoor = new GarageDoor("Garage");
         Stereo stereo = new Stereo("Living Room");
 
-        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+//        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+//        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+//        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+//        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
 
         CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
         CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
@@ -31,8 +31,8 @@ public class RemoteLoader {
         StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
         StereoOffCommand stereoOff = new StereoOffCommand(stereo);
 
-        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+        remoteControl.setCommand(0, () -> livingRoomLight.on(), () -> livingRoomLight.off());
+        remoteControl.setCommand(1, kitchenLight::on, kitchenLight::off);
         remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
         remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
 
